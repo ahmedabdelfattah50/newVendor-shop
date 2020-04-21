2745a48f027836e0a349ec6f65a773737ec7e6c7
Ahmed Abdel-Fattah Abdel-Moaty <47077630+ahmedabdelfattah50@users.noreply.github.com>
2020-04-21 23:39:26 +0200

    Create footer.php

---
+++ admins/includes/templates/footer.php
@@ -0,0 +1,7 @@
+        <!-- ================= scripts files ================= -->
+        <script src="<?php echo $js_path; ?>jquery 3_4_1.min.js"></script>
+        <script src="<?php echo $js_path; ?>propper 1_16.min.js"></script>
+        <script src="<?php echo $js_path; ?>bootstrap 4_4_1.min.js"></script>   
+        <script src="<?php echo $js_path; ?>admin_script.js"></script>
+    </body>
+</html>
